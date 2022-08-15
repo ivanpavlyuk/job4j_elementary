de6f4570513e16f5898e458b9de0e4fd5e96a0e8
Ivan Pavlyuk <111151964+ivanpavlyuk@users.noreply.github.com>
2022-08-15 19:34:26 +0300

    4.6.1. Switch [#334070]

--- src/main/java/ru/job4j/condition/SwitchWeek.java
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -5,28 +5,28 @@ public class SwitchWeek {
         String name;
         switch (day) {
             case 1 :
-                name = "Понедельник";
+                name = "РџРѕРЅРµРґРµР»СЊРЅРёРє";
                 return name;
             case 2 :
-                name = "Вторник";
+                name = "Р’С‚РѕСЂРЅРёРє";
                 return name;
             case 3 :
-                name = "Среда";
+                name = "РЎСЂРµРґР°";
                 return name;
             case 4 :
-                name = "Четверг";
+                name = "Р§РµС‚РІРµСЂРі";
                 return name;
             case 5 :
-                name = "Пятница";
+                name = "РџСЏС‚РЅРёС†Р°";
                 return name;
             case 6 :
-                name = "Суббота";
+                name = "РЎСѓР±Р±РѕС‚Р°";
                 return name;
             case 7 :
-                name = "Воскресенье";
+                name = "Р’РѕСЃРєСЂРµСЃРµРЅСЊРµ";
                 return name;
             default :
-                name = "Ошибка";
+                name = "РћС€РёР±РєР°";
                 return name;
         }
     }

e3fc8035d81fa703a7ca71c6369c13ec6b2dd5c4
Ivan Pavlyuk <111151964+ivanpavlyuk@users.noreply.github.com>
2022-08-16 16:57:40 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,16 @@ public class ArrayDefinition {
 
         float[] prices = new float[40];
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Ivan Ivanov";
+        names[1] = "Petr Petrov";
+        names[2] = "Sidor Sidorov";
+        names[3] = "John Petrucci";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }

2300cd78af4a7b79795b5e234c93c8dce8e6842a
Ivan Pavlyuk <111151964+ivanpavlyuk@users.noreply.github.com>
2022-08-18 14:16:08 +0300

    6.2.2. Переставить элементы массива [#226795 #332985]

--- src/test/java/ru/job4j/array/SwitchArrayTest.java
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -8,27 +8,27 @@ public class SwitchArrayTest {
     public void whenSwap0to3() {
         int[] input = {1, 2, 3, 4};
         int source = 0;
-        int dest = input.length - 1;
+        int dest = 3;
         int[] result = SwitchArray.swap(input, source, dest);
         int[] expected = {4, 2, 3, 1};
         assertThat(result).containsExactly(expected);
     }
 
     @Test
-    public void whenSwap1toLengthMinus2() {
+    public void whenSwap1to2() {
         int[] input = {1, 2, 3, 4};
         int source = 1;
-        int dest = input.length - 2;
+        int dest = 2;
         int[] result = SwitchArray.swap(input, source, dest);
         int[] expected = {1, 3, 2, 4};
         assertThat(result).containsExactly(expected);
     }
 
     @Test
-    public void whenSwapLengthDivBy2toLengthDivBy2plus1() {
+    public void whenSwap5to6() {
         int[] input = {1, 2, 3, 4, 5, 7, 6, 8, 9, 10};
-        int source = input.length / 2;
-        int dest = input.length / 2 + 1;
+        int source = 5;
+        int dest = 6;
         int[] result = SwitchArray.swap(input, source, dest);
         int[] expected = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         assertThat(result).containsExactly(expected);

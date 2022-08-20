8c8c3c300adcfb28f75a9cedc89b285e644402e2
Ivan Pavlyuk <111151964+ivanpavlyuk@users.noreply.github.com>
2022-08-20 16:20:37 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,8 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        int result = left >= right ? left : right;
-        return result;
+    public static int max(int first, int second) {
+        return first >= second ? first : second;
     }
+
+    public static int max(int first, int second, int third) {
+        return third >= max(first, second) ? third : max(first, second);
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return fourth >= max(first, second, third) ? fourth : max(first, second, third);
+        }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -14,20 +14,23 @@ public class MaxTest {
     }
 
     @Test
-    public void whenMax2To1Then2() {
-        int left = 2;
-        int right = 1;
-        int result = Max.max(left, right);
-        int expected = 2;
+    public void whenMax2To1To3Then3() {
+        int first = 2;
+        int second = 1;
+        int third = 3;
+        int result = Max.max(first, second, third);
+        int expected = 3;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    public void whenMax2To2Then2() {
-        int left = 2;
-        int right = 2;
-        int result = Max.max(left, right);
-        int expected = 2;
+    public void whenMax2To2To2To5Then2() {
+        int first = 2;
+        int second = 2;
+        int third = 2;
+        int fourth = 5;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
 }
